3284f708463dc5dbe013878112b43451c80d111c
oudsoft <oudsoft@gmail.com>
2019-10-23 21:48:41 +0700

    ปปปป

--- app/service.js
+++ app/service.js
@@ -56,7 +56,7 @@ app.post('/messages', (req, res) => {
 	console.log(JSON.stringify(req.body))
 	console.log(req.session.id); // => WQ8JaxI5nGAgyXM2IT466_zLm_oRGsTa
 	console.log(JSON.stringify(req.body.subscription)); // => {"endpoint":"https://fcm.googleapis.com/fcm/send/e8eoejqPMQ8:APA91bFifgREW-rZIfeiq0LA-vcPhcAzzcg6JPzoDIo5-xttjs6XbBRJanHkZnlOkd9_86Ew6xM3YXwARRz974H2ecuB72LZ74LdcrNd2LeqZ6po_H_2xWc7ENzq3TNVaY_ykjYbY_5J","expirationTime":null,"keys":{"p256dh":"BH1-84jK4GWmI5BGzgkjeFH53zOEdfD4fksYcmJYwfsjYVEZPpazsjenfFcc6u9DaxhXQD2K77Of3yKfKwcbNxQ","auth":"6a6P9keTMY794bH0PIOzxA"}}
-	const subscription = req.body.subscription;
+	const subscription = JSON.parse(req.body.subscription);
 
 	res.status(201).json({});
 
