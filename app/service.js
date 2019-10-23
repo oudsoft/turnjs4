8b64649a03f6e2582ee46e7d9040ac2b1c16c636
oudsoft <oudsoft@gmail.com>
2019-10-23 20:17:49 +0700

    okl

--- app/service.js
+++ app/service.js
@@ -29,10 +29,12 @@ webPush.setVapidDetails('mailto:test@example.com', publicVapidKey, privateVapidK
 
 app.get('/', function(req, res) {
 	console.log(JSON.stringify(req.body));
+	res.status(200).json({code: 200});
 })
 
 app.post('/registernewroom', (req, res) => {
 	console.log(JSON.stringify(req.body));
+	res.status(200).json({code: 200});	
 });
 
 
